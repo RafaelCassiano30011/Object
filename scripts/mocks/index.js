5cbd1f2613d2112eb603035e185bd83ced846d7e
Rafael Cassiano <77588479+RafaelCassiano30011@users.noreply.github.com>
2021-03-24 14:17:26 -0300

    Add files via upload

--- scripts/mocks/index.js
+++ scripts/mocks/index.js
@@ -6,17 +6,24 @@ const listaDeUsuarios = [{
         email: 'rafael30011@gmail.com'
     },
     {
-        image: 'images/homem1.jpg',
-        nome: 'Rafael',
-        segundoNome: 'Cassiano',
-        whatsapp: '11952075782',
-        email: 'rafael30011@gmail.com'
-    }, {
         image: 'images/homem2.jpg',
-        nome: 'Rafael',
-        segundoNome: 'Cassiano',
-        whatsapp: '11952075782',
-        email: 'rafael30011@gmail.com'
+        nome: 'Caio',
+        segundoNome: 'Pereira',
+        whatsapp: '11953065683',
+        email: 'Caio0011@gmail.com'
+    }, {
+        image: 'images/mulher1.jpg',
+        nome: 'Rafaela',
+        segundoNome: 'De Souza',
+        whatsapp: '11954074784',
+        email: 'rafaelasouza@gmail.com'
+    },
+    {
+        image: 'images/mulher2.jpg',
+        nome: 'Raquel',
+        segundoNome: 'Silva',
+        whatsapp: '11956026286',
+        email: 'raquelasiuva11@gmail.com'
     },
 ]
 
